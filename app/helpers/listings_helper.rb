0767a038aac5b1f16a05abf83e739c88b5563938
Robert Thomas <robertthomas@outlook.com.au>
2020-05-14 14:52:09 +1000

    added helpers in controller to convert integers into sale_type and sizes for views

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,32 @@
 module ListingsHelper
+
+  # def price_convert(price)
+  #   # sprintf "%.2f", price.to_f / 100
+  # end
+  
+  def number_to_size(size)
+    case size
+    when 1
+      'X-Small'
+    when 2 
+      'Small'
+    when 3
+      'Medium'
+    when 4
+      'Large'
+    when 5
+      'X-Large'
+    end
+  end
+
+  def number_to_sale_type(sale_type)
+    case sale_type
+    when 1
+      'For Sale'
+    when 2
+      'Swap/Trade'
+    when 3
+      'Free'
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-
+puts "---------------------------------"
+puts "Making Users who make Terrariums"
 i = 1
 2.times do User.create!( 
   email: "#{i}@gmail.com", 
@@ -7,70 +8,72 @@ i = 1
 )
   i += 1
 end
-
-puts "Users Created 👦👩"
-
+puts "Users Created 👉🌱👈"
+puts "---------------------------------"
+puts "Planting Seeds in Terrariums"
+puts "---------------------------------"
 User.find(1).listings.create(
-  title: "Medium",
-  price: 3000,
-  description: "This is a test terrarium for testing the application",
+  title: "Steps in a Bowl",
+  price: 30,
+  description: "Created using a bowl found in a thrift shop and materials collected while out on a nature walk, organised to looks like a set of stairs in the countryside.",
   sale_type: 1,
-  size: 2,
+  size: 3,
   image: '/images/med1.jpg'
 )
-
+puts "🌱"
 User.find(1).listings.create(
-  title: "Test-Terrarium-2",
-  price: 15000,
-  description: "This is a test terrarium for testing the application",
+  title: "Jungle Jar",
+  price: 45,
+  description: "A large cookie jar filled with ferns, sticks and stones sporting an overgrown jungle look",
   sale_type: 1,
-  size: 5,
+  size: 4,
   image: '/images/large1.jpg'
 )
-
+puts "🌱"
 User.find(1).listings.create(
-  title: "Test-Terrarium-3",
-  price: nil,
-  description: "This is a test terrarium for testing the application",
-  sale_type: 2,
-  size: 3,
+  title: "Abduction!",
+  price: 35,
+  description: "Mason jar with polymer clay sculptures of crash landing in a field with alien looking at a cow",
+  sale_type: 1,
+  size: 2,
   image: '/images/small1.jpg'
 )
-
+puts "🌱"
 User.find(1).listings.create(
-  title: "Test-Terrarium-4",
+  title: "Life on Venus",
   price: nil,
-  description: "This is a test terrarium for testing the application",
-  sale_type: 3,
+  description: "Small bowl with venus fly trap, I grow venus fly traps and have many terrariums filled with them! Looking to trade to add some variety to my collection.",
+  sale_type: 2,
   size: 2,
   image: '/images/small2.jpg'
 )
-
+puts "🌱"
 User.find(2).listings.create(
-  title: "Test-Terrarium-5",
-  price: nil,
-  description: "This is a test terrarium for testing the application",
-  sale_type: 3,
-  size: 2,
+  title: "Octagon",
+  price: 900,
+  description: "Very large octaganle terrarium with built in lighting structure.",
+  sale_type: 1,
+  size: 5,
   image: '/images/xlarge1.jpg'
 )
-
+puts "🌱"
 User.find(2).listings.create(
-  title: "Test-Terrarium-6",
-  price: nil,
-  description: "This is a test terrarium for testing the application",
-  sale_type: 3,
-  size: 2,
+  title: "Overgrown",
+  price: 300,
+  description: "Un-opened in years this 50 year old terrarium has only one surviving plant left that has strangled out all it's neighbors and filled the entire jar.",
+  sale_type: 1,
+  size: 5,
   image: '/images/xlarge2.jpg'
 )
-
+puts "🌱"
 User.find(2).listings.create(
-  title: "Test-Terrarium-7",
-  price: nil,
-  description: "This is a test terrarium for testing the application",
-  sale_type: 3,
-  size: 2,
+  title: "Tiny Terrariums!",
+  price: 10,
+  description: "I make tiny cute terrariums that you can fit anywhere, grab one fore your home, office or home office!",
+  sale_type: 1,
+  size: 1,
   image: '/images/xsmall1.jpg'
 )
-
-puts "Terrarium's Created 🌱"
+puts "---------------------------------"
+puts "🌱 Terrarium's Created 🌱"
+puts "---------------------------------"
