c24dae16d981c3345e62f937c18f394aaee9d699
Robert Thomas <robertthomas@outlook.com.au>
2020-05-15 14:29:21 +1000

    created profiles controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,32 @@
+class ProfilesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
   get '/', to: 'listings#redirect'
   resources :listings
+  resources :profiles
 end
